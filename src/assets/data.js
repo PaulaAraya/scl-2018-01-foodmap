3343c13d418c55103914ceb0c31f96957b66956e
PaulaAraya <paula.arayawilliams@gmail.com>
2018-07-26 18:25:13 -0300

    geolocalizacion

--- src/assets/data.js
+++ src/assets/data.js
@@ -18,6 +18,94 @@ const printInfo = (discover) => {
     (discover.results.items[0].title)}`
 };
 
+// Define search parameters:
+var params = {
+  // Look for places matching the category "eat and drink":
+  'cat': 'eat-drink',
 
+  'in': `${currentPosition.coords.longitude}, ${currentPosition.coords.latitude} `
+};
+
+const btn = document.querySelector("button");
+//creo evento para la busqueda del input 
+btn.addEventListener('click', () => {
+  const input = searchFood.value;
+  input = ' ';
+  if (input === '') {
+    alert("Por favor, debe introducir texto");
+  } else {
+    params;
+  }
+});
+
+
+// Define a callback function to handle errors:
+function onError(data) {
+  error = data;
+}
+
+
+
+
+// Run a search request with parameters, headers (empty), and callback functions:
+explore.request(params, {}, onResult, onError);
+
+
+  // Obtain the default map types from the platform object
+  //var maptypes = platform.createDefaultLayers();
+
+
+
+/*
+var defaultLayers = platform.createDefaultLayers();
+var mapPlaceholder = document.getElementById('map-container');
+
+//var map = new H.Map(
+//  mapContainer,
+//  defaultLayers.normal.map);
+//Tamaños mapa en difrentes pantallas
+window.addEventListener('resize', function () {
+  map.getViewPort().resize();
+});
+
+//Punto central del mapa y el nivel de zoom inicial del mapa. 
+var coordinates = {
+  lat: -33.43727, // Aquí estamos en santgo Centro
+  lng: -70.65056
+};
+
+var mapOptions = {
+  center: coordinates,
+  zoom: 14
+}
+
+var map = new H.Map(
+  mapContainer,
+  defaultLayers.normal.map,
+  mapOptions);
+
+//Interacción mapa
+var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));  
+
+//geolocalizacion watchPosition cada vez que cambie la posición del dispositivo.
+function updatePosition (event) {
+  var HEREHQcoordinates = {
+    lat: event.coords.latitude,
+    lng: event.coords.longitude
+  };
+console.log(HEREHQcoordinates)
+
+  var marker = new H.map.Marker(HEREHQcoordinates);
+  map.addObject(marker);
+  map.setCenter(HEREHQcoordinates);
+}
+
+navigator.geolocation.watchPosition(updatePosition);
+
+//agregar marcador nuevo
+var marker = new H.map.Marker(coordinates);
+map.addObject(marker);
+
+//clear map y poner pines*/
 
 
--- src/assets/main.js
+++ src/assets/main.js
@@ -6,71 +6,25 @@ const platform = new H.service.Platform({
 
 // Obtain the default map types from the platform object:
 var defaultLayers = platform.createDefaultLayers();
+// se declaran variables vacias que tomen requerimientos
+let currentPosition = null;
+let map = null;
 
-// Instantiate (and display) a map object:
-var map = new H.Map(
+//geolocalizar
+navigator.geolocation.getCurrentPosition((position)=>{
+  currentPosition = position;
+  // Instantiate (and display) a map object:
+  map = new H.Map(
   document.getElementById('mapContainer'),
   defaultLayers.normal.map,
   {
     zoom: 10,
     //busca posición actual del usuario
-    center: { lng: currentPosition.coords.longitude, 
-      lat: currentPosition.coords.latitude}
+    center: {
+      lng: currentPosition.coords.longitude,
+      lat: currentPosition.coords.latitude
+    }
   });
-
-  // Obtain the default map types from the platform object
-  //var maptypes = platform.createDefaultLayers();
-
-
-
-/*
-var defaultLayers = platform.createDefaultLayers();
-var mapPlaceholder = document.getElementById('map-container');
-
-//var map = new H.Map(
-//  mapContainer,
-//  defaultLayers.normal.map);
-//Tamaños mapa en difrentes pantallas
-window.addEventListener('resize', function () {
-  map.getViewPort().resize();
-});
-
-//Punto central del mapa y el nivel de zoom inicial del mapa. 
-var coordinates = {
-  lat: -33.43727, // Aquí estamos en santgo Centro
-  lng: -70.65056
-};
-
-var mapOptions = {
-  center: coordinates,
-  zoom: 14
-}
-
-var map = new H.Map(
-  mapContainer,
-  defaultLayers.normal.map,
-  mapOptions);
-
-//Interacción mapa
-var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));  
-
-//geolocalizacion watchPosition cada vez que cambie la posición del dispositivo.
-function updatePosition (event) {
-  var HEREHQcoordinates = {
-    lat: event.coords.latitude,
-    lng: event.coords.longitude
-  };
-console.log(HEREHQcoordinates)
-
-  var marker = new H.map.Marker(HEREHQcoordinates);
-  map.addObject(marker);
-  map.setCenter(HEREHQcoordinates);
-}
-
-navigator.geolocation.watchPosition(updatePosition);
-
-//agregar marcador nuevo
-var marker = new H.map.Marker(coordinates);
-map.addObject(marker);
-
-//clear map y poner pines*/
+})
+// variable Explore envia solicitudes de búsqueda
+var explore = new H.places.Explore(platform.getPlacesService()), exploreResult, error;
