3c8ad118553066207f7d8696e9504725130677a9
PaulaAraya <paula.arayawilliams@gmail.com>
2018-07-26 22:08:44 -0300

    no terminado

--- src/assets/main.js
+++ src/assets/main.js
@@ -11,68 +11,59 @@ let currentPosition = null;
 let map = null;
 
 //geolocalizar
-navigator.geolocation.getCurrentPosition((position)=>{
+navigator.geolocation.getCurrentPosition((position) => {
   currentPosition = position;
   // Instantiate (and display) a map object:
   map = new H.Map(
-  document.getElementById('mapContainer'),
-  defaultLayers.normal.map,
-  {
-    zoom: 15,
-    //busca posición actual del usuario
-    center: {
-      lng: currentPosition.coords.longitude,
-      lat: currentPosition.coords.latitude
-    }
-  });
+    document.getElementById('mapContainer'),
+    defaultLayers.normal.map,
+    {
+      zoom: 11,
+      //busca posición actual del usuario
+      center: {
+        lng: currentPosition.coords.longitude,
+        lat: currentPosition.coords.latitude
+      }
+    });
   console.log(position);
 })
 // variable Explore envia solicitudes de búsqueda
-var explore = new H.places.Explore(platform.getPlacesService()), exploreResult, error;
+var search = new H.places.Search(platform.getPlacesService()), searchResult, error;
+let markers = [];
 
-// Define search parameters:
-var params = {
-  // Look for places matching the category "eat and drink":
-  'cat': 'eat-drink',
+exampleFormControlSelect2.addEventListener('change', () => {
+  // Define search parameters:
+  var params = {
+    // Look for places matching the category "eat and drink":
+    'q': exampleFormControlSelect2.value,
 
-  'in': `${currentPosition.coords.longitude},${currentPosition.coords.latitude}`
-};
+    'at': `${currentPosition.coords.longitude},${currentPosition.coords.latitude}`
+  };
 
-const select = document.querySelector("select");
-//creo evento para la busqueda en un select
-select.addEventListener('click', () => {
-    params;
-});
-
-// Run a search request with parameters, headers (empty), and callback functions:
-explore.request(params, {}, onResult, onError);
-
-// Define a callback function to handle data on success:
-function onResult(data) {
-  exploreResult = data;
-  map.removeObject(markers); //remueve marcadores cuando cambias de geolocalización
-  markers = []; //almacena los marcadores 
-  //recorrer items para la info de los restaurantes
-  exploreResult.results.items.forEach(item => {
-  var icon = new H.map.Icon('../assets/img/icon/iconLocation.png');
-  let coords = {
-    lng: item.position[0],
-    lat: item.position[1],
-    direction: item.vicinity,
-    title: item.title
+  // Define a callback function to handle data on success:
+  function onResult(exploreResult) {//remueve marcadores cuando cambias de geolocalización
+    markers = []; //almacena los marcadores 
+    //recorrer items para la info de los restaurantes
+    console.log(exploreResult);
+    exploreResult.results.items.forEach(item => {
+      var icon = new H.map.Icon('./assets/img/icon/iconLocation.png');
+      let coords = {
+        lng: item.position[0],
+        lat: item.position[1]
+      }
+      var marker = new H.map.Marker(coords, { icon: icon });
+      markers.push(marker);
+      map.addObject(marker);
+    });
   }
-  var marker = new H.map.Marker(coords, { icon: icon });
-  console.log();
-  markers.push(marker);
-  map.addObject(marker);
-});
-}
 
-// Define a callback function to handle errors:
-function onError(data) {
-  error = data;
-}
+  // Define a callback function to handle errors:
+  function onError(data) {
+    error = data;
+  }
 
-// Run a search request with parameters, headers (empty), and callback functions:
-explore.request(params, {}, onResult, onError);
-        
+  // Run a search request with parameters, headers (empty), and callback functions:
+  search.request(params, {}, onResult, onError);
+})
+
+
